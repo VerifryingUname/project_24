c4e073ceed0b759f0f7063414b4ed1b778871927
VerifryingUname <kingdagone1@gmail.com>
2025-03-04 18:36:13 +0700

    lab24_3.cpp completed

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 
 using namespace std;
+int count(int data[], int size) {
+    int unique_count = 0;
+    
+    for (int i = 0; i < size; i++) {
+        bool is_unique = true;
+        for (int j = 0; j < i; j++) {
+            if (data[i] == data[j]) {
+                is_unique = false;
+                break;
+            }
+        }
+        if (is_unique) unique_count++;
+    }
+    
+    return unique_count;
+}
 
-//Write function count() here.
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
